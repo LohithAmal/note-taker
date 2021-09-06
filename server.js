f5f281ad09cfebcd854b507862f83e7f1dae3780
lohith <lohith05amal@gmail.com>
2021-09-05 22:24:18 -0400

    add middleware

--- server.js
+++ server.js
@@ -16,6 +16,19 @@ const PORT = process.env.PORT || 3000;
 
 // variable connect to json file
 const {notes} = require('./Develop/db/db.json');
+const { request } = require('http');
+const { response } = require('express');
 // new router to handle request
 const router = require('express').Router();
+
+// add middle ware
+app.use(express.urlencoded({extended:"true"}));
+app.use(express.json());
+app.use(express.static('./Develop/public'))
+// this code will get notes from json file db
+app.get('/api/notes', (request,response)=>{
+  response.json(notes)
+});
+
+
 
