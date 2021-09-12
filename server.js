d2aab4b45faaa7122702d3b3610361f7a4ce881e
lohith <lohith05amal@gmail.com>
2021-09-12 15:56:30 -0400

    code check and update

--- server.js
+++ server.js
@@ -1,23 +1,21 @@
 // call variables to connect dependency path
 
 const express = require ('express');
-
+const fs = require('fs');
 // Executing the function stored in express variable
 // And storing the result into app variable 
 const app = express()
 
 // adding path antive utility module
 const path = require('path');
-// file system module
-const fs = require ('fs');
 
 // use port 3000 unless there exists a preconfigured port
 const PORT = process.env.PORT || 3000;
 
 // variable connect to json file
 const {notes} = require('./Develop/db/db.json');
-const { request } = require('http');
-const { response } = require('express');
+const http = require('http');
+
 // new router to handle request
 const router = require('express').Router();
 
@@ -26,32 +24,34 @@ app.use(express.urlencoded({extended:"true"}));
 app.use(express.json());
 app.use(express.static('./Develop/public'))
 // this code will get notes from json file db
-app.get('/api/notes', (request,response)=>{
-  response.json(notes)
+app.get('/api/notes', (req,res)=>{
+  res.json(notes);
 });
 
 // this code will save notes to json
-app.post('/api/notes', (request, response)=>{
+app.post('/api/notes', (req, res)=>{
 
-  request.body.id = notes.length.toString();
-  const note = request.body;
+  req.body.id = notes.length.toString();
+  const note = req.body;
   notes.push(note)
   fs.writeFileSync(
     path.join(__dirname, "./Develop/db/db.json"),
     JSON.stringify({notes:notes}, null, 2)
   );
-  response.json(note)
-});
-// connect the backend to frontend
-app.get('/', (request,response)=>{
-  response.sendFile(path.join(__dirname,'./Develop/public/index.html'));
+  res.json(note)
 });
 
-app.get('/notes', (request, response)=>{
-  response.sendFile(path.join(__dirname,"./Develop/public/notes.html"));
+
+// connect the backend to frontend
+app.get('/', (req,res)=>{
+  res.sendFile(path.join(__dirname,'./Develop/public/index.html'));
+});
+
+app.get('/notes', (req, res)=>{
+  res.sendFile(path.join(__dirname,"./Develop/public/notes.html"));
 })
 
 app.listen(PORT, ()=>{
-  console.log(`server available on port ${PORT}`);
+  console.log(`API available on port ${PORT}`);
 });
 
