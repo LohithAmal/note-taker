3f1b807dca895c6f3e6afb95dd81badd0778a390
lohith <lohith05amal@gmail.com>
2021-09-12 16:26:19 -0400

    final file

--- server.js
+++ server.js
@@ -1,36 +1,26 @@
 // call variables to connect dependency path
-
 const express = require ('express');
 const fs = require('fs');
 // Executing the function stored in express variable
 // And storing the result into app variable 
 const app = express()
-
-// adding path antive utility module
+// file imports
 const path = require('path');
-
 // use port 3000 unless there exists a preconfigured port
 const PORT = process.env.PORT || 3000;
-
 // variable connect to json file
 const {notes} = require('./Develop/db/db.json');
 const http = require('http');
-
 // new router to handle request
 const router = require('express').Router();
-
 // add middle ware
 app.use(express.urlencoded({extended:"true"}));
 app.use(express.json());
 app.use(express.static('./Develop/public'))
-// this code will get notes from json file db
 app.get('/api/notes', (req,res)=>{
   res.json(notes);
 });
-
-// this code will save notes to json
 app.post('/api/notes', (req, res)=>{
-
   req.body.id = notes.length.toString();
   const note = req.body;
   notes.push(note)
@@ -40,16 +30,12 @@ app.post('/api/notes', (req, res)=>{
   );
   res.json(note)
 });
-
-
-// connect the backend to frontend
 app.get('/', (req,res)=>{
   res.sendFile(path.join(__dirname,'./Develop/public/index.html'));
 });
-
 app.get('/notes', (req, res)=>{
   res.sendFile(path.join(__dirname,"./Develop/public/notes.html"));
-})
+});
 
 app.listen(PORT, ()=>{
   console.log(`API available on port ${PORT}`);
